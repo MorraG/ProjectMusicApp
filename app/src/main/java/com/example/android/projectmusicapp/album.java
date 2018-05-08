f2b170607cbcf292b2415e05bc2a146b38e14492
MorraG <guidomorra13@gmail.com>
2018-05-08 20:38:42 +0200

    Review1

---
+++ app/src/main/java/com/example/android/projectmusicapp/album.java
@@ -0,0 +1,12 @@
+package com.example.android.projectmusicapp;
+
+import android.os.Bundle;
+
+public class album extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.album);
+    }
+}
